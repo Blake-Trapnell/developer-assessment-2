e0c8d89837fd6a00aa51db66b93c3395f8e341fe
Blake Trapnell <blake.trapnell103@gmail.com>
2021-04-21 21:20:51 -0600

    added images to loop

--- src/components/chapman-cares.vue
+++ src/components/chapman-cares.vue
@@ -21,6 +21,9 @@
         <div v-for="(event, index) in events" :key="index" class="event">
             <h3 class="event-name">{{ event.name }}</h3>
             <p class="event-text">{{ event.text }}</p>
+            <img 
+                v-bind:src='event.img'
+            />
         </div>
     </div>
 </template>
@@ -40,11 +43,13 @@ export default {
                     name: 'Making Strides Against Cancer',
                     text:
                         'Thousands of people, young and old, participated in the Making Strides Against Breast Cancer event at Tempe Beach Park. As a sponsorship partner, Chapman Auto brought the Breast Cancer Pace Car, driven by Danica Patrick. Chapman Auto employees participated in the walk, helping to raise thousands of dollars for breast cancer research.',
+                    img: require('@/assets/making-strides.jpg'),
                 },
                 {
                     name: 'Canine Companions For Independence',
                     text:
                         'Chapman Auto has been a proud supporter of Canine Companions for Independence for more than 10 years. From sponsoring a golf tournament to puppy training, Chapman Auto employees are proud to work with CCI as they help others live happy, independent lives.',
+                    img: require('@/assets/canine-companions.jpg'),
                 },
             ],
         };
